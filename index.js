0867fc9d2f7f4bcd3ca4fb1939768ab4efee0e10
arnab-shuvo <arnabshuvo430@gmail.com>
2020-06-24 22:50:27 +0600

    Swagger added

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const expressValidator = require('express-validator');
 const passport = require('passport');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 require('./auth/auth');
 
 dotenv.config();
 
 // db connection
 mongoose
-	.connect('mongodb://mongo:27017/node-mongo', {
+	.connect('mongodb://localhost:27017/node-mongo', {
 		useNewUrlParser: true,
 	})
 	.then(() => console.log('DB Connected'))
@@ -22,7 +24,8 @@ const app = express();
 //bring in routes
 
 const router = require('./routes');
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(expressValidator());
